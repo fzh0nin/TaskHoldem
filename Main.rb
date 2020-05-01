6ab2aa0c40150bc3215e55146b19800b0c54b4a5
fzhonin <yevhenii.yadrov@gmail.com>
2020-05-01 17:02:15 +0300

    bug fixed

--- Main.rb
+++ Main.rb
@@ -1,4 +1,16 @@
 require_relative  'Card.rb'
 require_relative  'Deck.rb'
 #require_relative 'DeckAnalyzer'
+puts'nachalo'
 deck1 = Deck.new
+puts 'Deck new'
+deck1.hand_initialize
+puts 'deck1 hand initialize'
+deck1.table_initialize
+puts'deck1 table initialize'
+deck1.deck_ranks_and_suits_set
+puts 'deck1.deck_ranks_and_suits_set'
+deck1.print_hand
+puts 'deck1.print_hand'
+deck1.print_table
+puts 'deck1.print_table'
